17da5ce9cac16bc72d87ac3d8a1ec33641f03186
tsifton <tdoginthehouse@gmail.com>
2017-04-04 17:00:11 -0600

    Added base classes

--- src/edu/neumont/Game.java
+++ src/edu/neumont/Game.java
@@ -5,16 +5,39 @@ package edu.neumont;
  */
 public class Game {
 
+    private Board board = new Board();
     private Player p1, p2;
+    private Player currentPlayer;
+    private Player loser;
 
-    public Game(Player player1, Player player2)
-    {
+    private boolean isGameOver = false;
+
+    public Game(Player player1, Player player2) {
         p1 = player1;
         p2 = player2;
+        currentPlayer = p1;
     }
 
-    public void Play()
-    {
-        
+    public void Play() {
+        while(!isGameOver) {
+            currentPlayer.makeMove(board);
+            SwapCurrentPlayer();
+            /*
+            if (!board.piecesLeft()) {
+                loser = currentPlayer;
+                isGameOver = true;
+            }
+             */
+        }
+
+        DisplayResults();
+    }
+
+    private void DisplayResults() {
+
+    }
+
+    private void SwapCurrentPlayer() {
+        currentPlayer = (currentPlayer == p1) ? p2 : p1;
     }
 }
--- src/edu/neumont/Main.java
+++ src/edu/neumont/Main.java
@@ -3,6 +3,7 @@ package edu.neumont;
 public class Main {
 
     public static void main(String[] args) {
-
+        Game game = new Game(new HumanPlayer(), new HumanPlayer());
+        game.Play();
     }
 }
