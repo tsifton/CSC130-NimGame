dd10cb4b1f4a11c7bcc9dd3810b6f42595d819f0
tsifton <tdoginthehouse@gmail.com>
2017-04-04 16:28:44 -0600

    Added Game Interface

--- src/edu/neumont/Game.java
+++ src/edu/neumont/Game.java
@@ -4,4 +4,17 @@ package edu.neumont;
  * Created by Trevor on 4/4/2017.
  */
 public class Game {
+
+    private Player p1, p2;
+
+    public Game(Player player1, Player player2)
+    {
+        p1 = player1;
+        p2 = player2;
+    }
+
+    public void Play()
+    {
+        
+    }
 }
