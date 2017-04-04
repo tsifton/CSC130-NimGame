6621cd87e57e4428e2b77ed33b1167e07d84e778
ZFlem96 <zackzacharyfleming@gmail.com>
2017-04-04 16:27:31 -0600

    Board with methods

--- src/edu/neumont/Board.java
+++ src/edu/neumont/Board.java
@@ -4,4 +4,14 @@ package edu.neumont;
  * Created by Trevor on 4/4/2017.
  */
 public class Board {
+
+	public void displayBoard(){
+		
+	}
+	
+	public boolean removePiece(int row, int numofPieces){
+		boolean canRemovePiece = false;
+		return canRemovePiece;
+	}
+	
 }
