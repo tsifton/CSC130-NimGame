7976507bd9547101661200598e5fcec2179f739a
brandwand <brandwand@yahoo.com>
2017-04-05 15:23:01 -0600

    Got stuff done

--- src/edu/neumont/HumanPlayer.java
+++ src/edu/neumont/HumanPlayer.java
@@ -1,10 +1,31 @@
 package edu.neumont;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Trevor on 4/4/2017.
  */
 public class HumanPlayer implements Player{
     public void makeMove(Board board) {
+    	ArrayList<Integer> input = getInput();
+    	board.removePiece(input.get(0), input.get(1));
+    }
+    
+    private ArrayList<Integer> getInput() {
+    	ArrayList<Boolean> checkList = new ArrayList<Boolean>();
+    	ArrayList<Integer> values = new ArrayList<Integer>();
+    	Scanner scan = new Scanner(System.in);
+    	System.out.print("Which Row would you like to select from? ");
+    	boolean row = scan.hasNextInt();
+    	checkList.add(row);
+    	values.add(scan.nextInt());
+    	System.out.print("How many pieces do you want to remove from this row? ");
+    	Scanner scan2 = new Scanner(System.in);
+    	boolean numOfPieces = scan2.hasNextInt();
+    	checkList.add(numOfPieces);
+    	values.add(scan2.nextInt());
 
+    	return values;
     }
 }
--- src/edu/neumont/Main.java
+++ src/edu/neumont/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game(new HumanPlayer(), new HumanPlayer());
         game.Play();
+//    	HumanPlayer player = new HumanPlayer();
+//    	Board board = new Board();
+//    	player.makeMove(board);
     }
 }
