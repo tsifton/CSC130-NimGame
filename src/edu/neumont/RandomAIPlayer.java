ddcf297c77f4cf4c0ae8808e38250a5c8b858dc5
tsifton <tdoginthehouse@gmail.com>
2017-04-07 16:53:34 -0600

    Removed broken tests

--- src/edu/neumont/RandomAIPlayer.java
+++ src/edu/neumont/RandomAIPlayer.java
@@ -11,10 +11,24 @@ public class RandomAIPlayer implements Player {
     @Override
     public void makeMove(BoardState state) {
         Random rand = new Random();
-        ArrayList<Integer> validRows = state.getValidRows();
+        ArrayList<Integer> validRows = getValidRows(state);
         int row = validRows.get(rand.nextInt(validRows.size()));
         int pieces = rand.nextInt(board.getNumPiecesLeftInRow(row)) + 1;
         board.removePiece(row, pieces);
     }
 
+    private ArrayList<Integer> getValidRows(BoardState state)
+    {
+        ArrayList<Integer> validRows = new ArrayList<>(rows.length);
+        for(int i = 0; i < rows.length; i++)
+        {
+            if(rows[i] != 0) validRows.add(i + 1);
+        }
+        return validRows;
+    }
+
+    private int getNumPiecesLeftInRow(BoardState state, int row)
+    {
+        return state.rows[row - 1];
+    }
 }
