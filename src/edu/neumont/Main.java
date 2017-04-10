11d6cad95ea9448af67e7a99c482db10cf41d40f
brandwand <brandwand@yahoo.com>
2017-04-10 16:46:18 -0600

    Added FIleIO with Write, and Read methods.

--- src/edu/neumont/Main.java
+++ src/edu/neumont/Main.java
@@ -1,9 +1,17 @@
 package edu.neumont;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Game game = new Game(new RandomAIPlayer(), new RandomAIPlayer());
         game.Play();
+        FileIO file = new FileIO();
+        BoardState state = new BoardState(new int[] {1,2,3});
+        ArrayList<BoardState> list = new ArrayList<BoardState>();
+        list.add(state);
+        file.writeToFile(list, "Bob.txt");
+        ArrayList<BoardState> newList = file.readFromFile("bob.txt");
     }
 }
