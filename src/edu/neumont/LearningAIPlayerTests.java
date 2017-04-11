4c72eaab8dc0e1d5e5994f87f8dc6724b526bc15
ZFlem96 <zackzacharyfleming@gmail.com>
2017-04-11 16:10:28 -0600

    comments in test

--- src/edu/neumont/LearningAIPlayerTests.java
+++ src/edu/neumont/LearningAIPlayerTests.java
@@ -1,17 +1,17 @@
 package edu.neumont;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class LearningAIPlayerTests {
 
 	@Test
 	public void LastMoveTest() {
-	    BoardState state = new BoardState(new int[]{3, 0, 0});
-
-
+		BoardState state = new BoardState(new int[] { 3, 0, 0 });
 		fail("Not yet implemented");
+		// test for case of 3 pieces left
+		// test for case of 2 pieces left
+
 	}
 
 }
