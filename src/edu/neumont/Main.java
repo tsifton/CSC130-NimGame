73548dd3cc2931feddb73b8e9130d039281e576b
brandwand <brandwand@yahoo.com>
2017-04-11 16:41:23 -0600

    Yo

--- src/edu/neumont/Main.java
+++ src/edu/neumont/Main.java
@@ -7,11 +7,5 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game(new RandomAIPlayer(), new RandomAIPlayer());
         game.Play();
-        FileIO file = new FileIO();
-        BoardState state = new BoardState(new int[] {1,2,3});
-        ArrayList<BoardState> list = new ArrayList<BoardState>();
-        list.add(state);
-        file.writeToFile(list, "Bob.txt");
-        ArrayList<BoardState> newList = file.readFromFile("bob.txt");
     }
 }
