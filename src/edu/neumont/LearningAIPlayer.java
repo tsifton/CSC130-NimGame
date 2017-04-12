5d370b7d1187cb6f57eb5dc5083f81a8e52d4df5
brandwand <brandwand@yahoo.com>
2017-04-12 16:01:28 -0600

    Started working on getting states

--- src/edu/neumont/LearningAIPlayer.java
+++ src/edu/neumont/LearningAIPlayer.java
@@ -1,6 +1,5 @@
 package edu.neumont;
 
-
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,44 +8,47 @@ import java.util.Random;
  */
 public class LearningAIPlayer implements Player {
 
-    private Random rand = new Random();
-    private StateHistory history;
+	private Random rand = new Random();
+	private StateHistory history;
 
-    public LearningAIPlayer(StateHistory history)
-    {
-        this.history = history;
-    }
+	public LearningAIPlayer(StateHistory history) {
+		this.history = history;
+	}
 
-    @Override
-    public void makeMove(Board board) {
-        if (history != null)
-        {
-            ArrayList<BoardState> stateHistory = history.getStates();
-            ArrayList<BoardState> possibleStates = findPossibleStates(stateHistory);
-        }
-    }
+	@Override
+	public void makeMove(Board board) {
+		if (history != null) {
+			ArrayList<BoardState> stateHistory = history.getStates();
+			ArrayList<BoardState> possibleStates = findPossibleStates(stateHistory);
+		}
+	}
 
-    private ArrayList<BoardState> findPossibleStates(ArrayList<BoardState> states)
-    {
+	private ArrayList<BoardState> findPossibleStates(ArrayList<BoardState> states) {
+		ArrayList<BoardState> list = new ArrayList<BoardState>();
+		for (int state = 0; state < list.size(); state++) {
+			for (int values = 0; values < states.get(state).rows.length; values++) {
+				if (states.get(state).rows[values] > /*Current State*/) {
+					
+				}
+			}
+		}
+		return list;
+	}
 
-    }
+	private void makeRandomMove(Board board) {
+		BoardState state = board.getState();
+		ArrayList<Integer> validRows = getValidRows(state);
+		int row = validRows.get(rand.nextInt(validRows.size()));
+		int pieces = rand.nextInt(state.rows[row]) + 1;
+		board.removePieces(row, pieces);
+	}
 
-    private void makeRandomMove(Board board)
-    {
-        BoardState state = board.getState();
-        ArrayList<Integer> validRows = getValidRows(state);
-        int row = validRows.get(rand.nextInt(validRows.size()));
-        int pieces = rand.nextInt(state.rows[row]) + 1;
-        board.removePieces(row, pieces);
-    }
-
-    private ArrayList<Integer> getValidRows(BoardState state)
-    {
-        ArrayList<Integer> validRows = new ArrayList<>();
-        for(int i = 0; i < state.rows.length; i++)
-        {
-            if(state.rows[i] != 0) validRows.add(i);
-        }
-        return validRows;
-    }
+	private ArrayList<Integer> getValidRows(BoardState state) {
+		ArrayList<Integer> validRows = new ArrayList<>();
+		for (int i = 0; i < state.rows.length; i++) {
+			if (state.rows[i] != 0)
+				validRows.add(i);
+		}
+		return validRows;
+	}
 }
