b7f906514e249ace8d8406290c0d208adcd27c29
brandwand <brandwand@yahoo.com>
2017-04-13 12:12:39 -0600

    Fixed Merge Problems. All Trevors Fault

--- src/edu/neumont/LearningAIPlayerTests.java
+++ src/edu/neumont/LearningAIPlayerTests.java
@@ -7,64 +7,43 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-
 public class LearningAIPlayerTests {
 
 	@Test
 	public void LearningAIPlayerTest() throws Exception {
-	    // Arrange
-        Board board = new Board(2, 0, 0);
-	    FileIO file = new FileIO();
-	    StateHistory history = new StateHistory(file.readFromFile("AIHistory"));
+		// Arrange
+		Board board = new Board(2, 0, 0);
+		FileIO file = new FileIO();
+		StateHistory history = new StateHistory(file.readFromFile("AIHistory"));
 		LearningAIPlayer ai = new LearningAIPlayer(history);
-	    BoardState expectedState = new BoardState(new int[] {1,0,0});
+		BoardState expectedState = new BoardState(new int[] { 1, 0, 0 });
 
-	    // Act
-        ai.makeMove(board);
+		// Act
+		ai.makeMove(board);
 
-		//Assert
-        assertArrayEquals(board.getState().rows, expectedState.rows);
+		// Assert
+		assertArrayEquals(board.getState().rows, expectedState.rows);
 	}
-	
 
 	@Test
 	public void writeToFileTest() {
 		FileIO fileIO = new FileIO();
-		BoardState state = new BoardState(new int[] {1,5,7});
+		BoardState state = new BoardState(new int[] { 1, 5, 7 });
 		ArrayList<BoardState> list = new ArrayList<BoardState>();
 		list.add(state);
 		fileIO.writeToFile(list, "testFile");
 		File file = new File("testFile");
 		assertTrue(file.exists());
 	}
-	
+
 	@Test
 	public void readFromFileTest() {
 		FileIO fileIO = new FileIO();
-		BoardState state = new BoardState(new int[] {1,5,7});
-		BoardState state2 = new BoardState(new int[] {1,3,7});
-		BoardState state3 = new BoardState(new int[] {1,2,7});
+		BoardState state = new BoardState(new int[] { 1, 5, 7 });
+		BoardState state2 = new BoardState(new int[] { 1, 3, 7 });
+		BoardState state3 = new BoardState(new int[] { 1, 2, 7 });
 
 		ArrayList<BoardState> list = new ArrayList<BoardState>();
-    @Test
-    public void RemoveInvalidNumberOfPiecesTest() throws Exception {
-        // Arrange
-        Board board = new Board(3, 5, 7);
-
-        // Act
-        boolean result = board.removePieces(0, 4);
-
-        //Assert
-        assertFalse(result);
-    }
-
-    @Test
-    public void RemovePiecesFromInvalidRowTest() throws Exception {
-        // Arrange
-        Board board = new Board(3, 5, 7);
-
-        // Act
-        boolean result = board.removePieces(4, 3);
 		list.add(state);
 		list.add(state2);
 		list.add(state3);
@@ -73,7 +52,29 @@ public class LearningAIPlayerTests {
 		assertEquals(list.size(), readState.size());
 	}
 
-        //Assert
-        assertFalse(result);
-    }
+	@Test
+	public void RemoveInvalidNumberOfPiecesTest() throws Exception {
+		// Arrange
+		Board board = new Board(3, 5, 7);
+
+		// Act
+		boolean result = board.removePieces(0, 4);
+
+		// Assert
+		assertFalse(result);
+	}
+
+	@Test
+	public void RemovePiecesFromInvalidRowTest() throws Exception {
+		// Arrange
+		Board board = new Board(3, 5, 7);
+
+		// Act
+		boolean result = board.removePieces(4, 3);
+
+		// Assert
+		assertFalse(result);
+
+	}
+
 }
