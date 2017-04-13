6b20224111ab5331f8acc438f0545fa0acf7bb05
brandwand <brandwand@yahoo.com>
2017-04-13 12:07:02 -0600

    Made Some Tests

--- src/edu/neumont/LearningAIPlayerTests.java
+++ src/edu/neumont/LearningAIPlayerTests.java
@@ -1,7 +1,12 @@
 package edu.neumont;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
 
 public class LearningAIPlayerTests {
 
@@ -19,5 +24,32 @@ public class LearningAIPlayerTests {
 
 		//Assert
         assertArrayEquals(board.getState().rows, expectedState.rows);
+	}
+	
+	@Test
+	public void writeToFileTest() {
+		FileIO fileIO = new FileIO();
+		BoardState state = new BoardState(new int[] {1,5,7});
+		ArrayList<BoardState> list = new ArrayList<BoardState>();
+		list.add(state);
+		fileIO.writeToFile(list, "testFile");
+		File file = new File("testFile");
+		assertTrue(file.exists());
+	}
+	
+	@Test
+	public void readFromFileTest() {
+		FileIO fileIO = new FileIO();
+		BoardState state = new BoardState(new int[] {1,5,7});
+		BoardState state2 = new BoardState(new int[] {1,3,7});
+		BoardState state3 = new BoardState(new int[] {1,2,7});
+
+		ArrayList<BoardState> list = new ArrayList<BoardState>();
+		list.add(state);
+		list.add(state2);
+		list.add(state3);
+		fileIO.writeToFile(list, "testFile2");
+		ArrayList<BoardState> readState = fileIO.readFromFile("testFile2");
+		assertEquals(list.size(), readState.size());
 	}
 }
